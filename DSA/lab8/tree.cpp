3eac39b3a2339a60df09ee98c6223135c00fa46a
Asim Poudel <asimdkt63@gmail.com>
2025-07-10 11:59:05 +0545

    Create tree.cpp

---
+++ DSA/lab8/tree.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    Node* left;
+    Node* right;
+    int data;
+    
+    Node(int val):left(NULL),right(NULL),data(val){}
+};
+
+void in_Order(Node *root){
+    if(root == NULL) return;
+    in_Order(root->left);
+    cout<<root->data<<" ";
+    in_Order(root->right);
+}
+
+void pre_Order(Node* root){
+    if(root == NULL) return;
+    cout<<root->data<<" ";
+    pre_Order(root->left);
+    pre_Order(root->right);
+}
+void post_Order(Node* root){
+    if(root == NULL) return;
+    pre_Order(root->left);
+    pre_Order(root->right);
+    cout<<root->data<<" ";
+}
+
+int main(){
+    Node* root = new Node(10);
+    root->left = new Node(20);
+    root->right = new Node(30);
+    root->left->left = new Node(40);
+    
+    cout<<"In-order Traversal: ";
+    in_Order(root);
+    
+    cout<<"\nPre-order Traversal: ";
+    pre_Order(root);
+    
+    cout<<"\nPost-order Traversal: ";
+    post_Order(root);
+    
+    return 0;
+}
